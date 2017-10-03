54a0625830fe3a81ddac7f0951c991e9b90b519e
tiwipabmin <tiwipabmin@gmail.com>
2017-10-03 22:11:29 +0700

    choose cinema and choose a movie time

--- lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/MainActivity.java
+++ lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/MainActivity.java
@@ -1,13 +1,55 @@
 package kmitl.temporary.ticketeasy;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import kmitl.temporary.ticketeasy.fragment.MovieFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_chooseCinema;
+    private AlertDialog.Builder chooseCinema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.movieFragment, new MovieFragment()).commit();
+
+        chooseCinema = new AlertDialog.Builder(this);
+        chooseCinema.setItems(R.array.cinema, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                switch (which){
+                    case 0: btn_chooseCinema.setText("Seacon");
+                        break;
+                    case 1: btn_chooseCinema.setText("Siam");
+                        break;
+                    default: break;
+                }
+            }
+        });
+
+        bindWidget();
+        setWidgetEventListener();
+    }
+
+    private void bindWidget(){
+        btn_chooseCinema = (Button) findViewById(R.id.btn_chooseCinema);
+    }
+
+    private void setWidgetEventListener(){
+        btn_chooseCinema.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseCinema.create();
+                chooseCinema.show();
+            }
+        });
     }
 }
---
+++ lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/fragment/MovieFragment.java
@@ -0,0 +1,107 @@
+package kmitl.temporary.ticketeasy.fragment;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import kmitl.temporary.ticketeasy.Main2Activity;
+import kmitl.temporary.ticketeasy.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MovieFragment extends Fragment {
+
+    private ImageView iv_movie;
+    private Button btn_time1, btn_time2, btn_time3, btn_time4, btn_time5;
+    private Intent intent;
+
+
+    public MovieFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_movie, container, false);
+        bindWidget(rootView);
+        setWidgetEventListener();
+        return rootView;
+    }
+
+    private void bindWidget(View rootView){
+
+        iv_movie = rootView.findViewById(R.id.iv_movie);
+
+        btn_time1 = rootView.findViewById(R.id.btn_time1);
+        btn_time2 = rootView.findViewById(R.id.btn_time2);
+        btn_time3 = rootView.findViewById(R.id.btn_time3);
+        btn_time4 = rootView.findViewById(R.id.btn_time4);
+        btn_time5 = rootView.findViewById(R.id.btn_time5);
+    }
+
+    private void setWidgetEventListener(){
+        btn_time1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getActivity(), Main2Activity.class);
+                intent.putExtra("time", btn_time1.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btn_time2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getActivity(), Main2Activity.class);
+                intent.putExtra("time", btn_time2.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btn_time3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getActivity(), Main2Activity.class);
+                intent.putExtra("time", btn_time3.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btn_time4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getActivity(), Main2Activity.class);
+                intent.putExtra("time", btn_time4.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+        btn_time5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getActivity(), Main2Activity.class);
+                intent.putExtra("time", btn_time5.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static MovieFragment newInstance() {
+
+        Bundle args = new Bundle();
+        MovieFragment fragment = new MovieFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+}
