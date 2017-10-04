aa38a23eb2fa7c9aefc0ab9b168a8ea8661fb41b
Student <student@LAB308-43.it.kmitl.ac.th>
2017-10-04 17:44:04 +0700

    create seat pic and add layout for seat_selection

--- lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/SeatSelectionActivity.java
+++ lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/SeatSelectionActivity.java
@@ -2,12 +2,23 @@ package kmitl.temporary.ticketeasy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SeatSelectionActivity extends AppCompatActivity {
-
+    private Map<String, Integer> map = new HashMap<String, Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_selection);
+        Button seat1 = (Button) findViewById(R.id.seat_1);
+        Button seat2 = (Button) findViewById(R.id.seat_2);
+
     }
+    public void onSelect(){
+
+    }
+
 }
