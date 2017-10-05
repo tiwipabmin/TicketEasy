fba242d26485903c0549d569efade6f26062138b
Student <student@LAB308-35.it.kmitl.ac.th>
2017-10-05 17:28:23 +0700

    get data from MovieFragment and send it out

--- lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/SeatSelectionActivity.java
+++ lab/lab06/TicketEasy/app/src/main/java/kmitl/temporary/ticketeasy/SeatSelectionActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +15,9 @@ import java.util.Map;
 public class SeatSelectionActivity extends AppCompatActivity {
     private ArrayList<Button> listSeat;
     private ArrayList<Integer> listStatus;
+    private ArrayList<Integer> listSelect;
+    private Intent intent;
+    private String time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +25,10 @@ public class SeatSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_seat_selection);
         listSeat = new ArrayList<Button>();
         listStatus = new ArrayList<Integer>();
-        Button seat1 = (Button) findViewById(R.id.seat_1);
-        Button seat2 = (Button) findViewById(R.id.seat_2);
-        Button seat3 = (Button) findViewById(R.id.seat_3);
-        Button seat4 = (Button) findViewById(R.id.seat_4);
-        Button seat5 = (Button) findViewById(R.id.seat_5);
-        Button seat6 = (Button) findViewById(R.id.seat_6);
-        Button seat7 = (Button) findViewById(R.id.seat_7);
-        Button seat8 = (Button) findViewById(R.id.seat_8);
-        Button seat9 = (Button) findViewById(R.id.seat_9);
-        Button seat10 = (Button) findViewById(R.id.seat_10);
-        listSeat.add(seat1);
-        listSeat.add(seat2);
-        listSeat.add(seat3);
-        listSeat.add(seat4);
-        listSeat.add(seat5);
-        listSeat.add(seat6);
-        listSeat.add(seat7);
-        listSeat.add(seat8);
-        listSeat.add(seat9);
-        listSeat.add(seat10);
+        listSelect = new ArrayList<Integer>();
+        setButton();
+        setTime();
+
 
         for(int x=0; x<listSeat.size();x++){
             listStatus.add(0);
@@ -66,5 +54,46 @@ public class SeatSelectionActivity extends AppCompatActivity {
     }
 
 
+    public void onComfirm(View view) {
+        for(int x=0;x<listSeat.size();x++){
+            if(listStatus.get(x) == 1){
+                listSelect.add(x);
+            }
+        }
+        intent = new Intent(SeatSelectionActivity.this, BillActivity.class);
+        intent.putExtra("seat", listSelect);
+        intent.putExtra("time", time);
+        startActivity(intent);
 
+    }
+    public void setButton(){
+        Button seat1 = (Button) findViewById(R.id.seat_1);
+        Button seat2 = (Button) findViewById(R.id.seat_2);
+        Button seat3 = (Button) findViewById(R.id.seat_3);
+        Button seat4 = (Button) findViewById(R.id.seat_4);
+        Button seat5 = (Button) findViewById(R.id.seat_5);
+        Button seat6 = (Button) findViewById(R.id.seat_6);
+        Button seat7 = (Button) findViewById(R.id.seat_7);
+        Button seat8 = (Button) findViewById(R.id.seat_8);
+        Button seat9 = (Button) findViewById(R.id.seat_9);
+        Button seat10 = (Button) findViewById(R.id.seat_10);
+
+        listSeat.add(seat1);
+        listSeat.add(seat2);
+        listSeat.add(seat3);
+        listSeat.add(seat4);
+        listSeat.add(seat5);
+        listSeat.add(seat6);
+        listSeat.add(seat7);
+        listSeat.add(seat8);
+        listSeat.add(seat9);
+        listSeat.add(seat10);
+    }
+
+    public void setTime() {
+        TextView time = (TextView) findViewById(R.id.textTimeMovie);
+        this.time = getIntent().getStringExtra("time");
+        time.setText(this.time.toString());
+
+    }
 }
